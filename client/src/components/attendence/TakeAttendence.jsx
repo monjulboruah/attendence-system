ace722572fecde959dd16ebea7ebba5d0ae1e55b
Joysankar <saikiajoysankar@gmail.com>
2023-03-29 22:27:01 +0530

    absent issue fix

--- client/src/components/attendence/TakeAttendence.jsx
+++ client/src/components/attendence/TakeAttendence.jsx
@@ -1,36 +1,38 @@
 import React from "react";
 import Webcam from "react-webcam";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import SendIcon from '@mui/icons-material/Send';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import SendIcon from "@mui/icons-material/Send";
+import { fontFamily } from "@mui/system";
 
-
-const studentData = [
-  {
-    schId: 23,
-    name: "Monjul Boruah"
-  },
-  {
-    schId: 29,
-    name: "Joysankar Saikia"
-  },
-  {
-    schId: 59,
-    name: "Suraj Jyoti Hazarika"
-  }
-]
+const studentData = [23,29,59];
 export default function TakeAttendence() {
   const webcamRef = React.useRef(null);
   const [imgSrc, setImgSrc] = React.useState(null);
-  const[pres, setPres] = useState([])
-  
+  const [pres, setPres] = useState([]);
+  const [absent, setAbsent] = useState([]);
 
   const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const constraints = { video: true };
+
+    async function getMedia() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        webcamRef.current.srcObject = stream;
+      } catch (error) {
+        console.error("Error accessing camera:", error);
+      }
+    }
+
+    getMedia();
+  }, []);
 
   const capture = React.useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -38,87 +40,100 @@ export default function TakeAttendence() {
   }, [webcamRef, setImgSrc]);
 
   const onSubmit = async (e) => {
-    setLoading(true)
+    setLoading(true);
     e.preventDefault();
     // let form_data = new FormData();
-    const img = imgSrc.replace(/^data:image\/(jpg);base64,/, "") ;
-    console.log(img)
-    
-   const data = {
-    img: img
-   }
+    const img = imgSrc.replace(/^data:image\/(jpg);base64,/, "");
+    console.log(img);
 
-
+    const data = {
+      img: img,
+    };
 
     fetch("http://127.0.0.1:5000/upload-test-image", {
       method: "POST",
       headers: {
-      'Content-Type' : 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(data)
-      }).then((res) => {
-        res.json().then((data)=> {
-          console.log(data)
-          setLoading(false)
-          setPres(data.schId)
-          console.log(data)
-        })
-      })
-      
-   
+      body: JSON.stringify(data),
+    }).then((res) => {
+      return res.json();
+    }).then((data) => {
+      console.log(data);
+      let filteredArray = []
+      filteredArray = studentData.filter(element => !data.schId.includes(element))
+      setAbsent(filteredArray)
+      setLoading(false);
+      setPres(data.schId);
+      console.log(filteredArray)
+    });
   };
 
- // console.log(imgSrc);
+  // console.log(imgSrc);
   return (
     <>
       {loading === false ? (
         <>
-
-        <Box sx={{ flexGrow: 1 }}>
-          <Grid container justifyContent="center">
+          <Box
+            sx={{ flexGrow: 1 }}
+            style={{ margin: "12px", fontFamily: "fantasy" }}
+          >
+            <Grid container justifyContent="center">
               <Grid item>
-              <h1 style={{color: "black"}}>Capture image of the class for taking attendance</h1>
+                <h1 style={{ color: "black" }}>
+                  Capture image for taking attendance
+                </h1>
               </Grid>
-          </Grid>
-        </Box>
-             
+            </Grid>
+          </Box>
 
           <Box sx={{ flexGrow: 1, padding: 5 }}>
-              <Grid container justifyContent="center">
-                <Grid item>
-                  <Webcam
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Webcam
                   audio={false}
                   ref={webcamRef}
                   forceScreenshotSourceSize
                   videoConstraints={{
                     height: 500,
-                    width: 500
+                    width: 500,
                   }}
                   screenshotFormat="image/jpeg"
-                  />
-                </Grid>
+                />
+              </Grid>
             </Grid>
           </Box>
 
           <Box sx={{ flexGrow: 1 }}>
             <Stack spacing={2} justifyContent="center" direction="row">
-              <Button variant="contained" color="success" onClick={capture} endIcon={<CameraAltIcon />}>Capture</Button>
-              <Button variant="contained" onClick={onSubmit} endIcon={<SendIcon/>}>Send</Button>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={capture}
+                endIcon={<CameraAltIcon />}
+              >
+                Capture
+              </Button>
+              <Button
+                variant="contained"
+                onClick={onSubmit}
+                endIcon={<SendIcon />}
+              >
+                Send
+              </Button>
             </Stack>
           </Box>
 
           <Box sx={{ flexGrow: 1 }}>
-          <Grid container justifyContent="center">
-          <h1 style={{color: "black"}}>Preview</h1>
-          </Grid>
-        </Box>
+            <Grid container justifyContent="center">
+              <h1 style={{ color: "black" }}>Preview</h1>
+            </Grid>
+          </Box>
 
           <Box sx={{ flexGrow: 1, padding: 5 }}>
-              <Grid container justifyContent="center">
-                <Grid item>
-                  {imgSrc && <img src={imgSrc} />}
-                </Grid>
-              </Grid>
+            <Grid container justifyContent="center">
+              <Grid item>{imgSrc && <img src={imgSrc} />}</Grid>
+            </Grid>
           </Box>
           <Box sx={{ flexGrow: 1, padding: 5 }}>
             <Grid container justifyContent="center">
@@ -126,25 +141,49 @@ export default function TakeAttendence() {
                 <h2>Students Present</h2>
               </Grid>
               <Grid item lg={12}>
-            {
-             pres.length !== 0 && pres.map((schId, idx)=> {
-                return(
-                  <div id={idx}>
-                   <p>{schId}</p>
-                   <p>{schId === 23 ? "Monjul Boruah" : schId === 29 ? "Joysankar Saikia" : schId === 59 ?
-              "Surajjyoti Hazarika" : "Unknown"}</p>
-                  </div>
-                )
-              })
-            }
+                {pres.length !== 0 &&
+                  pres.map((schId, idx) => {
+                    return (
+                      <div id={idx}>
+                        <p>{schId}</p>
+                        <p>
+                          {schId === 23
+                            ? "Monjul Boruah"
+                            : schId === 29
+                            ? "Joysankar Saikia"
+                            : schId === 59
+                            ? "Surajjyoti Hazarika"
+                            : "Unknown"}
+                        </p>
+                      </div>
+                    );
+                  })}
+              </Grid>
+              <Grid item lg={12}>
+                <h2>Students Absent</h2>
+              </Grid>
+              <Grid item lg={12}>
+              {absent.length !== 0 &&
+                  absent.map((schId, idx) => {
+                    return (
+                      <div id={idx}>
+                        <p>{schId}</p>
+                        <p>
+                          {schId === 23
+                            ? "Monjul Boruah"
+                            : schId === 29
+                            ? "Joysankar Saikia"
+                            : schId === 59
+                            ? "Surajjyoti Hazarika"
+                            : "Unknown"}
+                        </p>
+                      </div>
+                    );
+                  })}
               </Grid>
             </Grid>
           </Box>
         </>
-       
-
-       
-       
       ) : (
         <p>Loading... | Please wait</p>
       )}
--- client/src/components/student/CreateStrudents.jsx
+++ client/src/components/student/CreateStrudents.jsx
@@ -4,10 +4,11 @@ import axios from "axios";
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import {Link} from "@mui/material"
-import {GlobalState} from "../../GlobalState"
+import { Link } from "@mui/material"
+import { GlobalState } from "../../GlobalState"
 import AddIcon from '@mui/icons-material/Add';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import Grid from '@mui/material/Grid';
 
 const CreateStudent = () => {
 
@@ -16,82 +17,88 @@ const CreateStudent = () => {
   const [isLoggedIn] = state.isLoggedIn;
 
   const [studentData, setStudentData] = useState({
-   name: "",
-   scholarId: "",
-   branch: "",
-   subject: ""
+    name: "",
+    scholarId: "",
+    branch: "",
+    subject: ""
   });
 
 
   return (
-    <>
-    
-        <div className="new">
-      
-      <div className="newContainer">
-       
-        <div className="top">
-          <h1 style={{color: "black"}}>Add A Student</h1>
+    < >
+
+
+      <div className="new" >
+
+        <div className="newContainer">
+
+          <div className="top">
+            <h1 style={{ color: "black" }}>Add A Student</h1>
+          </div>
+          <div className="bottom">
+            <div className="right">
+              <form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={12} md={6} lg={6}>
+                    <div className="formInput">
+                      <label>Student Name</label>
+                      <input
+                        type="text"
+                        placeholder=""
+
+                      />
+                    </div></Grid>
+                  <Grid item xs={12} sm={12} md={6} lg={6}>
+                    <div className="formInput">
+                      <label>Scholar Id</label>
+                      <input type="text" placeholder="" />
+                    </div></Grid>
+                  <Grid item xs={12} sm={12} md={6} lg={6}>
+                    <div className="formInput">
+                      <label>Branch</label>
+                      <input
+                        type="text"
+                        placeholder=""
+
+                      />
+                    </div></Grid>
+
+
+                  <Grid item xs={12} sm={12} md={6} lg={6}>
+                    <div className="formInput">
+                      <label>Subjects Enrolled</label>
+                      <input
+                        type="text"
+                        placeholder=""
+
+                      />
+                    </div></Grid></Grid>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="bottom">
-          <div className="right">
-            <form>
-              <div className="formInput">
-                <label>Student Name</label>
-                <input
-                  type="text"
-                  placeholder=""
-                  
-                />
-              </div>
 
-              <div className="formInput">
-                <label>Scholar Id</label>
-                <input type="text" placeholder=""  />
-              </div>
 
-              <div className="formInput">
-                <label>Branch</label>
-                <input
-                  type="text"
-                  placeholder=""
-                  
-                />
-              </div>
 
-             
+      </div>
+      <div className="new">
+        <div className="newContainer">
+          <div className="top" style={{ justifyContent: "center" }}>
+            <div >
 
-              <div className="formInput">
-                <label>Subjects Enrolled</label>
-                <input
-                  type="text"
-                  placeholder=""
-                 
-                />
-              </div>
-            </form>
+              <Button variant="contained" color="success" endIcon={<AddIcon />} style={{ margin: "3px" }}>Add Student</Button>
+
+              <Button variant="contained" color="info" endIcon={<AddAPhotoIcon />} href="/add-training-data" style={{ margin: "3px" }}>Take Images</Button>
+
+            </div>
+
           </div>
         </div>
       </div>
-    
-       
-    
-    </div>
-    <div className="new">
-      <div className="newContainer">
-        <div className="top">
-          <div style={{paddingLeft: "450px"}}>
-          <Button variant="contained" color="success" endIcon={<AddIcon/>} style={{marginLeft: "12px"}}>Add Student</Button>
-          <Button variant="contained" color="info" endIcon={<AddAPhotoIcon/>} href="/add-training-data" style={{marginLeft: "12px"}}>Take Images</Button>
-          </div>
-          
-        </div>
-      </div>
-    </div>
-      </>
-   
-   
+    </>
+
+
   )
 };
 
-export default CreateStudent;
+export default CreateStudent
